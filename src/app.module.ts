8c0205a1596b47dc74e73983802683c4c9b49a5e
beauspot <beauspot@outlook.com>
2024-06-08 22:13:58 +0100

    refactored typeorm config and then added it to imports array

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,8 @@
 import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { parse } from "pg-connection-string";
-import * as dotenv from "dotenv";
-
-dotenv.config();
-
-const pgConnectionString = process.env.DB_CONNECTION_STRING;
-
-if (!pgConnectionString)
-  throw new Error("DB_CONNECTION_STRING is  not defined");
-
-const config = parse(pgConnectionString);
+import { TypeormConfig } from "./typeorm/typeorm.config";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: config.host,
-      port: parseInt(config.port, 10),
-      username: config.user,
-      password: config.password,
-      database: config.database,
-      entities: [],
-      ssl: {
-        rejectUnauthorized: false, // required for ssl connection
-      },
-      synchronize: true, // set to false in prod
-    }),
-  ],
+  imports: [TypeormConfig],
   controllers: [],
   providers: [],
 })
