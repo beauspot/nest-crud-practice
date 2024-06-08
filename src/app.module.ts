eaeefecde65654de54b2d48e018fb5f4ef56ca26
beauspot <beauspot@outlook.com>
2024-06-08 20:33:33 +0100

    finished setting up dotenv and also pg-connection string in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,33 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { parse } from "pg-connection-string";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const pgConnectionString = process.env.DB_CONNECTION_STRING;
+
+if (!pgConnectionString)
+  throw new Error("DB_CONNECTION_STRING is  not defined");
+
+const config = parse(pgConnectionString);
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: config.host,
+      port: parseInt(config.port, 10),
+      username: config.user,
+      password: config.password,
+      database: config.database,
+      entities: [],
+      ssl: {
+        rejectUnauthorized: false, // required for ssl connection
+      },
+      synchronize: true, // set to false in prod
+    }),
+  ],
   controllers: [],
   providers: [],
 })
