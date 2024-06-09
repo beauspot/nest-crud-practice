77c0c4ae539db6c098ef08472efc36b0e34fb7b5
beauspot <beauspot@outlook.com>
2024-06-09 11:52:49 +0100

    installed and implemented helmet for security

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
 import * as dotenv from "dotenv";
+import helmet from "helmet";
+
+import { AppModule } from "./app.module";
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   await app.listen(process.env.PORT);
 }
 bootstrap();
