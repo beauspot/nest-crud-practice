04267dbebe3e7468f0178118aef015b6a6865935
beauspot <beauspot@outlook.com>
2024-06-09 19:59:33 +0100

    finished setting up a create user payload for signing up a user

---
+++ src/users/utils/interface/createusers.payload.interface.ts
@@ -0,0 +1,10 @@
+export interface createusersinterface {
+  fullName: string;
+  country: string;
+  phoneNumber: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  transactionPin: string;
+  confirmTransactionPin: string;
+}
