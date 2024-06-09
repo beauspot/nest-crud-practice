c6239ecc1201d47504c9d9e5229749e833c5a84e
beauspot <beauspot@outlook.com>
2024-06-09 19:57:50 +0100

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.enableCors();
   await app.listen(process.env.PORT);
 }
 bootstrap();
