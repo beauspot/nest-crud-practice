3f239a471205cf090d21ee516f293ad636a32756
beauspot <beauspot@outlook.com>
2024-06-09 11:20:27 +0100

    finished setting up the users entity

--- src/typeorm/entities/users.entity.ts
+++ src/typeorm/entities/users.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  BeforeInsert,
+  BeforeUpdate,
+} from "typeorm";
+import { IsString, Length, validateOrReject } from "class-validator";
 
 @Entity({ name: "users" })
 export class User {
@@ -11,9 +18,32 @@ export class User {
   @Column({ unique: true, nullable: false })
   phoneNumber: string;
 
+  @Column({ unique: true, nullable: false })
+  email: string;
+
   @Column({ nullable: false })
   fullName: string;
 
-  @Column({ default: new Date() })
+  @Column({ nullable: false })
+  password: string;
+
+  @Column({ nullable: false })
+  confirmPassword: string;
+
+  @Column({ nullable: false })
+  @IsString()
+  transactionPin: string;
+
+  @Column({ nullable: false })
+  @IsString()
+  confirmTransactionPin: string;
+
+  @Column()
   createdAt: Date;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  async validate() {
+    await validateOrReject(this);
+  }
 }
